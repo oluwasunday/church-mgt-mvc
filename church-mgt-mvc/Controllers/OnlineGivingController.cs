acdd529995c8da57f6da34d05879870be47e2cc4
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-11-10 19:04:57 +0100

    Online giving implemented

--- church-mgt-mvc/Controllers/OnlineGivingController.cs
+++ church-mgt-mvc/Controllers/OnlineGivingController.cs
@@ -1,10 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using church_mgt_model.ViewModels;
+using church_mgt_services.interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace church_mgt_mvc.Controllers
 {
     public class OnlineGivingController : Controller
     {
+        private readonly IPaymentService _paymentService;
+
+        public OnlineGivingController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(OnlineGivingViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _paymentService.MakePaymentAsync(model);
+                if(result != null)
+                {
+                    return Redirect(result.Authorization_Url);
+                }
+            }
+            ViewBag.Error = "Error occur, try again!";
+            return View();
+        }
+
+        public IActionResult SuccessPayment()
         {
             return View();
         }
---
+++ church-mgt-services/interfaces/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using church_mgt_model.ViewModels;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace church_mgt_services.interfaces
+{
+    public interface IPaymentService
+    {
+        Task<OnlineGivingResponseViewModel> MakePaymentAsync(OnlineGivingViewModel model);
+    }
+}
