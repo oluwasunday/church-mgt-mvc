1e27e77590507c0ea7e4a9ad5e635a4cee9398cb
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-11-11 13:32:59 +0100

    home page clean up

---
+++ church-mgt-model/Dtos/AddTestimonyDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace church_mgt_model.Dtos
+{
+    public class AddTestimonyDto
+    {
+        [Required(ErrorMessage = "Please enter your full name")]
+        public string FullName { get; set; }
+
+        [Required]
+        public string Sex { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter your location")]
+        public string Address { get; set; }
+
+        [Required]
+        public string YourTestimony { get; set; }
+    }
+}
